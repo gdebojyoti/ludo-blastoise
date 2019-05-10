a861d3f7411373985573d1e1d0b2db405eb5823e
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-05-11 01:55:11 +0530

    Trigger event when new player joins.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const io = require('socket.io')()
 
+const matches = []
+const matchId = 'M31291' // some random match ID
+let playersCount = 0 // number of players joined since last server restart
+
 io.on('connection', (client) => {
+  console.log("new connection...")
+  const playerId = ++playersCount
+
   client.on('subscribeToTimer', (interval) => {
     console.log('client is subscribing to timer with interval ', interval)
     setInterval(() => {
       client.emit('timer', new Date())
     }, interval)
+  })
+
+  client.on('JOIN_MATCH', ({ name, home }) => {
+    console.log(name, "has joined")
+    io.emit('PLAYER_JOINED', {
+      matchId,
+      id: `P${playerId}`,
+      name,
+      home
+    })
   })
 
   client.on('disconnect', function () {
