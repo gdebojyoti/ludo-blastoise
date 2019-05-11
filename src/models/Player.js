aad4af6127ea032ab50a46f14a02976421140566
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-05-11 21:05:45 +0530

    Next coin position.

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,3 +1,4 @@
+const { getNextPosition } = require('../utilities/generic')
 const playerStatus = require('../constants/playerStatus')
 
 class Player {
@@ -8,7 +9,7 @@ class Player {
 
     const homeId = ['red', 'blue', 'yellow', 'green'].indexOf(home) + 1
     this.coins = {
-      alfa: homeId * 100 + 1,
+      alfa: homeId * 100 + 34,
       beta: homeId * 100 + 2,
       charlie: homeId * 100 + 3,
       delta: homeId * 100 + 4
@@ -16,7 +17,25 @@ class Player {
   }
 
   moveCoin (coinId, roll) {
-    this.coins[coinId] += roll
+    // this.coins[coinId] += roll
+    let position = this.coins[coinId]
+    const moves = []
+    const homeId = ['red', 'blue', 'yellow', 'green'].indexOf(this.home) + 1
+
+    while (roll > 0) {
+      // get next position
+      position = getNextPosition(position, homeId)
+
+      // add position to moves list
+      moves.push(position)
+
+      // decrease remaining roll count
+      roll--
+    }
+
+    console.log("moves", moves)
+
+    this.coins[coinId] = position
   }
 }
 
--- src/utilities/generic.js
+++ src/utilities/generic.js
@@ -14,7 +14,41 @@ function getDiceRollNumber () {
   return 2
 }
 
+function getNextPosition (currentPosition, homeId) {
+  // XYZ: quadrant ID = X; remaining: YZ
+  // calculate quadrant ID & remaining
+  const quadrantId = Math.floor(currentPosition / 100)
+  const remaining = currentPosition % 100
+
+  let nextPosition = 0
+  
+  if (remaining >= 10 && remaining < 15) {
+    nextPosition = currentPosition + 1
+  } else if (remaining === 15) {
+    nextPosition = quadrantId * 100 + 25
+  } else if (remaining === 25) {
+    nextPosition = quadrantId * 100 + (homeId === quadrantId ? 24 : 35)
+  } else if ((remaining > 20 && remaining < 25) || (remaining > 30 && remaining <= 35)) {
+    nextPosition = currentPosition - 1
+  } else if (remaining === 30) {
+    nextPosition = (quadrantId - 1) * 100 + 10
+    if (quadrantId === 1) {
+      nextPosition += 400
+    }
+  } else if (remaining === 20) {
+    nextPosition = quadrantId * 100 + 99
+  } else {
+    // stay in same position if all else fails
+    nextPosition = currentPosition
+  }
+
+  console.log("nextPosition", nextPosition, quadrantId)
+
+  return nextPosition
+}
+
 module.exports = {
   fetchItemFromArrayWithKey,
-  getDiceRollNumber
+  getDiceRollNumber,
+  getNextPosition
 }
