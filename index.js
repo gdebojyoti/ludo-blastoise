c92efd29874dc28918edc2eeafe81169bce999a9
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-05-11 16:03:34 +0530

    Test: Update coin position via interval.

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const matchId = 'M31291' // some random match ID
 let playersCount = 0 // number of players joined since last server restart
 
 io.on('connection', (client) => {
-  console.log("new connection...")
+  console.log("new connection...", client.id)
   const playerId = ++playersCount
+  let coinPosition = 100
+
+  const temp = {}
+
+  client.join(matchId)
 
   client.on('subscribeToTimer', (interval) => {
     console.log('client is subscribing to timer with interval ', interval)
@@ -15,18 +20,45 @@ io.on('connection', (client) => {
     }, interval)
   })
 
+  // when a new player joins, inform everyone
   client.on('JOIN_MATCH', ({ name, home }) => {
-    console.log(name, "has joined")
-    io.emit('PLAYER_JOINED', {
+    console.log(name, playerId, "has joined", home)
+    
+    // send details to joined player
+    client.emit('CLIENT_JOINED', {
+      id: `P${playerId}`,
+      name,
+      home,
+      matchId
+    })
+
+    // send new joinee details to others
+    client.to(matchId).emit('PLAYER_JOINED', {
       matchId,
       id: `P${playerId}`,
       name,
       home
     })
+
+    // test; update 'beta' coin position every 2 secs
+    temp.interval = setInterval(() => {
+      io.in(matchId).emit('UPDATE_COIN_POSITION', {
+        playerId: `P${playerId}`,
+        coinId: 'beta',
+        position: ++coinPosition
+      })
+    }, 5000)
   })
 
+  // when a player disconnects, inform others
   client.on('disconnect', function () {
-    io.emit('user disconnected')
+    console.log(playerId, "has left")
+    client.in(matchId).emit('PLAYER_LEFT', {
+      playerId: `P${playerId}`
+    })
+
+    // temp; delete interval when player disconnects
+    clearInterval(temp.interval)
   })
 })
 
