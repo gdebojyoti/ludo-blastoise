5dfc868182d94ab8756a1f641bb4fe8e272bda82
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-05-16 21:45:55 +0530

    Fake private members fixed in Match model.

--- src/models/Match.js
+++ src/models/Match.js
@@ -3,17 +3,6 @@ const matchStatus = require('../constants/matchStatus')
 const { getDiceRollNumber, getHomeId, getCoinIndex } = require('../utilities/generic')
 
 /*
- * Private members
- */
-
-let cells = {}
-const players = {} // list of all players in this match
-let _didEatEnemyCoin = false // becomes true if the player just ate an enemy coin
-let _coinJustReachedEnd = false // becomes true if a coin just reached end
-let _noSelectionPossible = false // becomes true if the player can not select any of their coins after rolling dice
-// let _hostPlayerId = ''
-
-/*
  * Class & Public methods
  */
 
@@ -24,24 +13,35 @@ class Match {
     this.status = matchStatus.PREMATCH
     this.lastRoll = 0 // value of previous dice roll; roll = spaces by which a coin will move
     this.isDiceRolled = false // whether or not current player already rolled the dice; prevents multiple dice rolls
+
+    /*
+     * Private members
+     */
+
+    this._cells = {}
+    this._players = {} // list of all players in this match
+    this._didEatEnemyCoin = false // becomes true if the player just ate an enemy coin
+    this._coinJustReachedEnd = false // becomes true if a coin just reached end
+    this._noSelectionPossible = false // becomes true if the player can not select any of their coins after rolling dice
+    // this._hostPlayerId = ''
   }
 
   getAllPlayers () {
-    return players
+    return this._players
   }
 
   // cell ID in which coin currently exists
   getCoinPosition (playerId, coinId) {
-    return players[playerId].coins[coinId]
+    return this._players[playerId].coins[coinId]
   }
 
   // home position of coin; eg: red alfa = 101, green charlie = 403
   getCoinHomePosition (playerId, coinId) {
-    return _getCoinHomePosition(playerId, coinId)
+    return this._getCoinHomePosition(playerId, coinId)
   }
 
   setNoSelectionPossible () {
-    _noSelectionPossible = true
+    this._noSelectionPossible = true
   }
 
   // check if player exists in match; return true if player ID ('id') is found in 'turns'
@@ -51,16 +51,16 @@ class Match {
 
   // declare GAME OVER if current player has all coins at X99
   isGameOver () {
-    const completedCoins = players[this.currentTurn].getCompletedCoins()
+    const completedCoins = this._players[this.currentTurn].getCompletedCoins()
     return completedCoins.length === 4
   }
 
   // add new player to match
   addPlayer (id, name, home) {
-    // create entry for player in 'players' list
-    players[id] = new Player(name, home)
+    // create entry for player in 'this._players' list
+    this._players[id] = new Player(name, home)
     console.log('new player added ---')
-    console.log(players[id])
+    console.log(this._players[id])
 
     this.updateTurns(id, home)
   }
@@ -71,7 +71,7 @@ class Match {
     const homeId = getHomeId(home)
     let i = 0
     while (i < this.turns.length) {
-      const player = players[this.turns[i]]
+      const player = this._players[this.turns[i]]
       const playerHomeId = getHomeId(player.getHome())
       if (playerHomeId > homeId) {
         break
@@ -89,25 +89,25 @@ class Match {
   // update and return next player's turn
   getNextTurn () {
     // continue turn for same player if they just ate an enemy coin
-    if (_didEatEnemyCoin) {
-      _didEatEnemyCoin = false // reset flag
+    if (this._didEatEnemyCoin) {
+      this._didEatEnemyCoin = false // reset flag
       return this.currentTurn
     }
 
     // continue turn for same player if their coin just reached its end
-    if (_coinJustReachedEnd) {
-      _coinJustReachedEnd = false
+    if (this._coinJustReachedEnd) {
+      this._coinJustReachedEnd = false
       return this.currentTurn
     }
 
     // continue turn for same player if they rolled a 6, and at least one possible coin could be selected
-    if (this.lastRoll === 6 && !_noSelectionPossible) {
+    if (this.lastRoll === 6 && !this._noSelectionPossible) {
       return this.currentTurn
     }
 
-    // reset _noSelectionPossible flag to false
-    if (_noSelectionPossible) {
-      _noSelectionPossible = false
+    // reset this._noSelectionPossible flag to false
+    if (this._noSelectionPossible) {
+      this._noSelectionPossible = false
     }
 
     const currentIndex = this.turns.indexOf(this.currentTurn)
@@ -122,7 +122,7 @@ class Match {
   }
 
   getCoinPath (playerId, coinId) {
-    return players[playerId].getCoinPath(coinId, this.lastRoll)
+    return this._players[playerId].getCoinPath(coinId, this.lastRoll)
   }
 
   playerMovesCoin (playerId, coinId, moves) {
@@ -132,45 +132,45 @@ class Match {
     }
 
     // fetch current position of coin
-    const currentPosition = players[playerId].getCoinPosition(coinId)
+    const currentPosition = this._players[playerId].getCoinPosition(coinId)
 
     // remove coin from above position in 'coinPositions'
-    _removeCoinFromCell(currentPosition, playerId, coinId)
+    this._removeCoinFromCell(currentPosition, playerId, coinId)
 
     // calculate new position
     const newPosition = moves.slice(-1)[0]
 
     // add coin to new position
-    _addCoinToCell(newPosition, playerId, coinId)
+    this._addCoinToCell(newPosition, playerId, coinId)
 
-    console.log('cells', cells)
+    console.log('cells', this._cells)
 
-    players[playerId].updateCoinPosition(coinId, newPosition)
+    this._players[playerId].updateCoinPosition(coinId, newPosition)
   }
 
   coinReachedEnd (playerId, coinId) {
-    _coinJustReachedEnd = true
-    players[playerId].coinReachedEnd(coinId)
+    this._coinJustReachedEnd = true
+    this._players[playerId].coinReachedEnd(coinId)
   }
 
   // try to eat enemy coin if not on a safe cell; if eaten, return coin details
   didEatEnemyCoin (position, playerId, coinId) {
-    if (_isOnSafeCell(position)) {
+    if (this._isOnSafeCell(position)) {
       return null
     }
 
     // check if enemy coin exists
-    const enemyCoin = _detectEnemyCoin(position, playerId, coinId)
+    const enemyCoin = this._detectEnemyCoin(position, playerId, coinId)
     if (enemyCoin) {
       // remove enemy coin
-      _eatEnemyCoin(enemyCoin, position)
+      this._eatEnemyCoin(enemyCoin, position)
 
       // update enemy coin's position
       const { playerId, coinId } = enemyCoin
-      players[playerId].updateCoinPosition(coinId, _getCoinHomePosition(playerId, coinId))
+      this._players[playerId].updateCoinPosition(coinId, this._getCoinHomePosition(playerId, coinId))
 
       // update status
-      _didEatEnemyCoin = true
+      this._didEatEnemyCoin = true
 
       return enemyCoin
     }
@@ -194,74 +194,74 @@ class Match {
   }
 
   getPossibleSelectionsForPlayer () {
-    return players[this.currentTurn].getPossibleSelections(this.lastRoll)
-  }
-}
-
-/*
- * Private methods
- */
-
-function _removeCoinFromCell (cellId, playerId, coinId) {
-  if (!cells[cellId] || !cells[cellId].length) {
-    return
+    return this._players[this.currentTurn].getPossibleSelections(this.lastRoll)
   }
 
-  const coinIndex = _getCoinIndexInCell(cellId, playerId, coinId)
+  /*
+   * Private methods
+   */
 
-  coinIndex > -1 && cells[cellId].splice(coinIndex, 1)
-}
-
-function _addCoinToCell (cellId, playerId, coinId) {
-  if (!cells[cellId]) {
-    cells[cellId] = []
-  }
-
-  const coinIndex = _getCoinIndexInCell(cellId, playerId, coinId)
-
-  coinIndex === -1 && cells[cellId].push({
-    playerId,
-    coinId
-  })
-}
-
-// check if enemy coins exist in given position; if so, return the first coin
-function _detectEnemyCoin (cellId, playerId) {
-  const enemyCoins = cells[cellId].filter(coin => coin.playerId !== playerId)
-  return enemyCoins[0]
-}
-
-// move enemy coin from its current position to home position
-function _eatEnemyCoin (coin, cellId) {
-  console.log('enemy coin', coin, cellId)
-  const { playerId, coinId } = coin
-  // remove enemy coin from current position
-  _removeCoinFromCell(cellId, playerId, coinId)
-  // add enemy coin to home position
-  const homePosition = _getCoinHomePosition(playerId, coinId)
-  _addCoinToCell(homePosition, playerId, coinId)
-}
-
-function _getCoinIndexInCell (cellId, playerId, coinId) {
-  let coinIndex = -1
-  cells[cellId].forEach((coin, index) => {
-    if (coinIndex === -1 && coin.playerId === playerId && coin.coinId === coinId) {
-      coinIndex = index
+  _removeCoinFromCell (cellId, playerId, coinId) {
+    if (!this._cells[cellId] || !this._cells[cellId].length) {
+      return
     }
-  })
-  return coinIndex
-}
 
-function _getCoinHomePosition (playerId, coinId) {
-  const home = players[playerId].getHome()
-  const homeId = getHomeId(home)
+    const coinIndex = this._getCoinIndexInCell(cellId, playerId, coinId)
 
-  return 100 * homeId + getCoinIndex(coinId)
-}
+    coinIndex > -1 && this._cells[cellId].splice(coinIndex, 1)
+  }
 
-// determine if given cell is a safe one (eg: 113, 134)
-function _isOnSafeCell (position) {
-  return position % 100 === 13 || position % 100 === 34
+  _addCoinToCell (cellId, playerId, coinId) {
+    if (!this._cells[cellId]) {
+      this._cells[cellId] = []
+    }
+
+    const coinIndex = this._getCoinIndexInCell(cellId, playerId, coinId)
+
+    coinIndex === -1 && this._cells[cellId].push({
+      playerId,
+      coinId
+    })
+  }
+
+  // check if enemy coins exist in given position; if so, return the first coin
+  _detectEnemyCoin (cellId, playerId) {
+    const enemyCoins = this._cells[cellId].filter(coin => coin.playerId !== playerId)
+    return enemyCoins[0]
+  }
+
+  // move enemy coin from its current position to home position
+  _eatEnemyCoin (coin, cellId) {
+    console.log('enemy coin', coin, cellId)
+    const { playerId, coinId } = coin
+    // remove enemy coin from current position
+    this._removeCoinFromCell(cellId, playerId, coinId)
+    // add enemy coin to home position
+    const homePosition = this._getCoinHomePosition(playerId, coinId)
+    this._addCoinToCell(homePosition, playerId, coinId)
+  }
+
+  _getCoinIndexInCell (cellId, playerId, coinId) {
+    let coinIndex = -1
+    this._cells[cellId].forEach((coin, index) => {
+      if (coinIndex === -1 && coin.playerId === playerId && coin.coinId === coinId) {
+        coinIndex = index
+      }
+    })
+    return coinIndex
+  }
+
+  _getCoinHomePosition (playerId, coinId) {
+    const home = this._players[playerId].getHome()
+    const homeId = getHomeId(home)
+
+    return 100 * homeId + getCoinIndex(coinId)
+  }
+
+  // determine if given cell is a safe one (eg: 113, 134)
+  _isOnSafeCell (position) {
+    return position % 100 === 13 || position % 100 === 34
+  }
 }
 
 module.exports = Match
